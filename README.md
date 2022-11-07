e46699841682e5f612a62f300f87047876f9810b
Eugene Roslyakov <ei.roslyakov@gmail.com>
2022-11-07 12:47:13 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# make_bill_report
+![workflow](https://github.com/ei-roslyakov/get_bill_data/actions/workflows/tests.yml/badge.svg)
+## Script for getting billing data from AWS Cost Explorer  
+## How to use  
+### Install dependencies
+
+```python
+pip3 install -r requirements.txt 
+```
+### To use AWS profile (profile must be configured, this option is only available with console report )
+```python
+python3 ./get_bill.py --profile=rei-prod --month=06 --year=2022 --report-to-console --no-report-to-file
+```
+
+### To make report and write it to the exel file
+#### This is the default behavior, the application will read information from an excel file in the folder _report_ (first two columns) and request billing data from AWS
+```python
+python3 ./get_bill.py --month=06 --year=2022
+```
+#### this command will add a new column for each project with date and amount
+
+### !!! Make sure that you have created a report file with projects and account ids (you can copy the template file), this file uses as source data.
+
+### Script arguments
+| Name         | Description                                       | Default    |
+|--------------|---------------------------------------------------|------------|
+| `--profile`  | AWS profile to get access to the Cost Explorer    | default    |
+| `--month`    | The report will be created for this month         | 01    |
+| `--year`     | The report will be created for this year          | 2022 |
+| `--report-to-file`   | If true, will create exel report in folder report | True      |
+| `--report-to-console`   | If true, will print report to console             | False      |
+
+
+### Exel output example
+![report](/files/report.jpg "report")
+### Console output example
+```
++-------------+-----------------------+--------+------+
+| ProjectName | TimePeriod            | Amount | Unit |
++-------------+-----------------------+--------+------+
+| rei-prod    | 2022:05:01-2022:06:01 | 5.69   | USD  |
++-------------+-----------------------+--------+------+
+
++-------------------------------------+-------+------+-------------------------+
+| Service                             | Total | Unit | TimePeriod              |
++-------------------------------------+-------+------+-------------------------+
+| AWS Key Management Service          | 0.0   | USD  | 2022-05-01 - 2022-06-01 |
+| Amazon EC2 Container Registry (ECR) | 0.0   | USD  | 2022-05-01 - 2022-06-01 |
+| Amazon Elastic Load Balancing       | 4.37  | USD  | 2022-05-01 - 2022-06-01 |
+| Amazon Route 53                     | 1.01  | USD  | 2022-05-01 - 2022-06-01 |
+| Amazon Simple Storage Service       | 0.32  | USD  | 2022-05-01 - 2022-06-01 |
+| AmazonCloudWatch                    | 0.0   | USD  | 2022-05-01 - 2022-06-01 |
+| Tax                                 | 0.0   | USD  | 2022-05-01 - 2022-06-01 |
++-------------------------------------+-------+------+-------------------------+
+```
---
+++ get_bill.py
@@ -0,0 +1,222 @@
+import argparse
+import os.path
+
+import boto3
+
+from botocore.exceptions import ClientError
+
+import loguru
+
+import pandas as pd
+
+from office365.runtime.auth.client_credential import ClientCredential
+from office365.sharepoint.client_context import ClientContext
+
+logger = loguru.logger
+
+
+REPORT_FILE_NAME = "report"
+
+
+def parse_args():
+    parsers = argparse.ArgumentParser()
+
+    parsers.add_argument(
+        "--profile",
+        required=False,
+        type=str,
+        default="default",
+        action="store",
+        help="AWS Profile",
+    )
+    parsers.add_argument(
+        "--region",
+        required=False,
+        type=str,
+        default="eu-west-2",
+        action="store",
+        help="AWS region",
+    )
+    parsers.add_argument(
+        "--month", required=False, type=str, default="01", action="store", help="Month"
+    )
+    parsers.add_argument(
+        "--year",
+        required=False,
+        type=str,
+        default="2022",
+        action="store",
+        help="The beginning of the time period",
+    )
+
+    return parsers.parse_args()
+
+
+def client_role(acc_id: str, region: str):
+
+    sts_client = boto3.client("sts")
+    assumed_role = sts_client.assume_role(
+        RoleArn=f"arn:aws:iam::{acc_id}:role/su-get-bill-data-access",
+        RoleSessionName="AssumeRoleSession1",
+        DurationSeconds=1800,
+    )
+    session = boto3.Session(
+        aws_access_key_id=assumed_role["Credentials"]["AccessKeyId"],
+        aws_secret_access_key=assumed_role["Credentials"]["SecretAccessKey"],
+        aws_session_token=assumed_role["Credentials"]["SessionToken"],
+        region_name=region,
+    )
+    ce_client = session.client("ce")
+
+    return ce_client
+
+
+def get_bill_by_period(ce_client, start: str, end: str, project="NoN") -> list:
+
+    logger.info(f"{project}: Getting data for period {start} - {end}")
+    try:
+        response = ce_client.get_cost_and_usage(
+            TimePeriod={"Start": start, "End": end},
+            Granularity="MONTHLY",
+            Metrics=["BlendedCost"],
+        )
+        return response
+    except ClientError as e:
+        logger.exception(f"Something went wrong {e.response['Error']['Message']}")
+
+
+def get_date_range(year: str, month: str):
+
+    # year = 2021
+    # month = 11
+
+    def check_month(month):
+        if month == "11" or month == "12":
+            return ""
+        else:
+            return "0"
+
+    suitable_value_for_month = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+    ]
+
+    if month not in suitable_value_for_month:
+        logger.exception(
+            f"You have provided the wrong month number {month}, available values are {suitable_value_for_month} "
+        )
+        exit(1)
+
+    if month == "01":
+        start = f"{int(year) - 1}-12-01"
+        end = f"{year}-{month}-01"
+
+        return {"start": start, "end": end}
+
+    if month != "01":
+        start = f"{year}-{check_month(month)}{int(month) - 1}-01"
+        end = f"{year}-{month}-01"
+
+        return {"start": start, "end": end}
+
+
+def make_report(year: str, month: str, region: str):
+    def sort_data_by_month(data):
+        sorted_data = []
+        for project in data:
+            project_with_acc = dict(list(project.items())[:2])
+            sorted_bill_by_month = {
+                k: dict(list(project.items())[2:])[k]
+                for k in sorted(dict(list(project.items())[2:]))
+            }
+            data = {**project_with_acc, **sorted_bill_by_month}
+            sorted_data.append(data)
+        return sorted_data
+
+    date_range = get_date_range(year, month)
+
+    df = pd.read_excel(f"report/{REPORT_FILE_NAME}.xlsx")
+
+    data_to_write = []
+    for item in df.to_dict("records"):
+        try:
+            ce_client = client_role(item["AccountID"], region)
+            data = get_bill_by_period(
+                ce_client, date_range["start"], date_range["end"], item["AccountName"]
+            )
+            for amount_value in data["ResultsByTime"]:
+                item[f"{month}.{year}"] = round(
+                    float(amount_value["Total"]["BlendedCost"]["Amount"]), 2
+                )
+                data_to_write.append(item)
+        except ClientError as e:
+            logger.exception(f"Something went wrong {e.response['Error']['Message']}")
+        except Exception as e:
+            logger.exception(f"Something went wrong {e}")
+
+    df = pd.DataFrame(sort_data_by_month(data_to_write))
+
+    try:
+        logger.info("Making file backup")
+        os.rename(
+            f"report/{REPORT_FILE_NAME}.xlsx", f"report/{REPORT_FILE_NAME}-backup.xlsx"
+        )
+        with pd.ExcelWriter(
+            f"report/{REPORT_FILE_NAME}.xlsx", engine="xlsxwriter"
+        ) as writer:
+            df.to_excel(writer, sheet_name=year, index=False)
+            for column in df:
+                column_width = (
+                    max(df[column].astype(str).map(len).max(), len(column)) + 3
+                )
+                col_idx = df.columns.get_loc(column)
+                writer.sheets[f"{year}"].set_column(col_idx, col_idx, column_width)
+
+            workbook = writer.book
+            worksheet = writer.sheets[year]
+            position = 0
+
+            for item in range(len(data_to_write)):
+                chart = workbook.add_chart({"type": "line"})
+                chart.add_series(
+                    {
+                        "categories": [f"{year}", 0, 2, 0, 15],
+                        "values": [f"{year}", 1 + item, 2, 1 + item, 15],
+                        "name": f"{df['AccountName'].values[item]}",
+                        "line": {"color": "red"},
+                    }
+                )
+                chart.set_x_axis({"name": "Month", "position_axis": "on_tick"})
+                chart.set_y_axis(
+                    {"name": "Price", "major_gridlines": {"visible": False}}
+                )
+                chart.set_legend({"position": "none"})
+                worksheet.insert_chart(f"O{20 + position}", chart)
+                position += 2
+
+    except Exception as e:
+        logger.exception(f"Something went wrong {e}")
+
+
+def main():
+
+    logger.info("Application started")
+    args = parse_args()
+
+    make_report(args.year, args.month, args.region)
+
+    logger.info("Application finished")
+
+
+if __name__ == "__main__":
+    main()
