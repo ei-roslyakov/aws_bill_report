4078edaf235fe8b641d00aa8394c034455c52aeb
Eugene Roslyakov <ei.roslyakov@gmail.com>
2022-11-08 18:00:37 +0200

    some impr

--- get_bill.py
+++ get_bill.py
@@ -8,6 +8,9 @@ from botocore.exceptions import ClientError
 import loguru
 
 import pandas as pd
+import calendar
+from boto3.dynamodb.conditions import Key, Attr
+
 
 logger = loguru.logger
 
@@ -68,6 +71,20 @@ def client_role(acc_id: str, region: str):
     return ce_client
 
 
+def dynamodb_client():
+
+    dynamodb_client = boto3.client("dynamodb")
+
+    return dynamodb_client
+
+
+def dynamodb_resource():
+
+    dynamodb_resource = boto3.resource("dynamodb")
+
+    return dynamodb_resource
+
+
 def get_bill_by_period(ce_client, start: str, end: str, project="NoN") -> list:
 
     logger.info(f"{project}: Getting data for period {start} - {end}")
@@ -83,7 +100,6 @@ def get_bill_by_period(ce_client, start: str, end: str, project="NoN") -> list:
 
 
 def get_date_range(year: str, month: str):
-
     def check_month(month):
         if month == "11" or month == "12":
             return ""
@@ -110,107 +126,149 @@ def get_date_range(year: str, month: str):
             f"You have provided the wrong month number {month}, available values are {suitable_value_for_month} "
         )
         exit(1)
+    start = None
+    end = None
 
-    if month == "01":
-        start = f"{int(year) - 1}-12-01"
-        end = f"{year}-{month}-01"
-
-        return {"start": start, "end": end}
-
-    if month != "01":
-        start = f"{year}-{check_month(month)}{int(month)}-01"
+    if month == "12":
+        start = f"{year}-{month}-01"
+        end = f"{int(year) + 1}-01-01"
+    elif month == "12":
+        start = start = f"{year}-{month}-01"
+        end = f"{year}-{int(month) + 1}-01"
+    else:
+        start = start = f"{year}-{month}-01"
         end = f"{year}-{check_month(month)}{int(month) + 1}-01"
 
-        return {"start": start, "end": end}
+    return {"start": start, "end": end}
 
 
-def make_report(year: str, month: str, region: str):
-    def sort_data_by_month(data):
-        sorted_data = []
-        for project in data:
-            project_with_acc = dict(list(project.items())[:2])
-            sorted_bill_by_month = {
-                k: dict(list(project.items())[2:])[k]
-                for k in sorted(dict(list(project.items())[2:]))
-            }
-            data = {**project_with_acc, **sorted_bill_by_month}
-            sorted_data.append(data)
-        return sorted_data
+def check_table_exists(resource, table_name: str) -> bool:
+    try:
+        resource.Table(table_name).table_status
+    except resource.meta.client.exceptions.ResourceNotFoundException:
+        return False
+    return True
 
-    date_range = get_date_range(year, month)
 
-    df = pd.read_excel(f"report/{REPORT_FILE_NAME}.xlsx")
+def create_table(dynamodb, table_name: str):
+    try:
+        response = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {"AttributeName": "Project", "KeyType": "HASH"},
+                {"AttributeName": "Id", "KeyType": "RANGE"},
+            ],
+            AttributeDefinitions=[
+                {"AttributeName": "Project", "AttributeType": "S"},
+                {"AttributeName": "Id", "AttributeType": "S"},
+            ],
+            ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+        )
+    except ClientError as err:
+        logger.error(
+            f"Something went wrong code: {err.response['Error']['Code']} \n message: {err.response['Error']['Message']}"
+        )
+        raise
 
-    data_to_write = []
-    for item in df.to_dict("records"):
-        try:
-            ce_client = client_role(item["AccountID"], region)
-            data = get_bill_by_period(
-                ce_client, date_range["start"], date_range["end"], item["AccountName"]
-            )
-            for amount_value in data["ResultsByTime"]:
-                item[f"{month}.{year}"] = round(
-                    float(amount_value["Total"]["BlendedCost"]["Amount"]), 2
-                )
-                data_to_write.append(item)
-        except ClientError as e:
-            logger.exception(f"Something went wrong {e.response['Error']['Message']}")
-        except Exception as e:
-            logger.exception(f"Something went wrong {e}")
+    logger.info(f"CREATED")
 
-    df = pd.DataFrame(sort_data_by_month(data_to_write))
+
+def put_data(dynamodb, project_name, project_id, month, bill, table):
+    table = dynamodb.Table(table)
 
     try:
-        logger.info("Making file backup")
-        os.rename(
-            f"report/{REPORT_FILE_NAME}.xlsx", f"report/{REPORT_FILE_NAME}-backup.xlsx"
+        response = table.update_item(
+            Key={
+                "Project": project_name,
+                "Id": project_id,
+            },
+            UpdateExpression=f"SET {calendar.month_name[int(month)]} = :m",
+            ExpressionAttributeValues={":m": bill},
+            ReturnValues="UPDATED_NEW",
         )
-        with pd.ExcelWriter(
-            f"report/{REPORT_FILE_NAME}.xlsx", engine="xlsxwriter"
-        ) as writer:
-            df.to_excel(writer, sheet_name=year, index=False)
-            for column in df:
-                column_width = (
-                    max(df[column].astype(str).map(len).max(), len(column)) + 3
-                )
-                col_idx = df.columns.get_loc(column)
-                writer.sheets[f"{year}"].set_column(col_idx, col_idx, column_width)
+    except ClientError as err:
+        logger.error(
+            f"Something went wrong code: {err.response['Error']['Code']} \n message: {err.response['Error']['Message']}"
+        )
+        raise
 
-            workbook = writer.book
-            worksheet = writer.sheets[year]
-            position = 0
-
-            for item in range(len(data_to_write)):
-                chart = workbook.add_chart({"type": "line"})
-                chart.add_series(
-                    {
-                        "categories": [f"{year}", 0, 2, 0, 15],
-                        "values": [f"{year}", 1 + item, 2, 1 + item, 15],
-                        "name": f"{df['AccountName'].values[item]}",
-                        "line": {"color": "red"},
-                    }
-                )
-                chart.set_x_axis({"name": "Month", "position_axis": "on_tick"})
-                chart.set_y_axis(
-                    {"name": "Price", "major_gridlines": {"visible": False}}
-                )
-                chart.set_legend({"position": "none"})
-                worksheet.insert_chart(f"O{20 + position}", chart)
-                position += 2
-
-    except Exception as e:
-        logger.exception(f"Something went wrong {e}")
+    logger.info(f"UPDATED: {response['ResponseMetadata']['HTTPStatusCode']}")
 
 
-def main():
+def get_projects_with_ids(dynamodb, table):
+
+    projects_with_ids = []
+
+    projects = scan_db(dynamodb, table)
+
+    for project in projects:
+        projects_with_ids.append({"Project": project["Project"], "Id": project["Id"]})
+
+    return projects_with_ids
+
+
+def scan_db(dynamodb, table, scan_kwargs=None):
+    if scan_kwargs is None:
+        scan_kwargs = {}
+
+    table = dynamodb.Table(table)
+
+    complete = False
+    records = []
+    while not complete:
+        try:
+            response = table.scan(**scan_kwargs)
+        except botocore.exceptions.ClientError as error:
+            raise Exception("Error quering DB: {}".format(error))
+
+        records.extend(response.get("Items", []))
+        next_key = response.get("LastEvaluatedKey")
+        scan_kwargs["ExclusiveStartKey"] = next_key
+
+        complete = True if next_key is None else False
+    return records
+
+
+def main(table_name):
 
     logger.info("Application started")
     args = parse_args()
 
-    make_report(args.year, args.month, args.region)
+    projects_with_ids = get_projects_with_ids(dynamodb_resource(), table_name)
+    date_range = get_date_range(args.year, args.month)
+
+    for project in projects_with_ids:
+        ce_client = client_role(project["Id"], args.region)
+        data = get_bill_by_period(
+            ce_client, date_range["start"], date_range["end"], project["Project"]
+        )
+        bill = round(
+            float(
+                [
+                    amount_value["Total"]["BlendedCost"]["Amount"]
+                    for amount_value in data["ResultsByTime"]
+                ][0]
+            ),
+            2,
+        )
+        put_data(
+            dynamodb_resource(),
+            project["Project"],
+            project["Id"],
+            args.month,
+            str(bill),
+            table_name,
+        )
 
     logger.info("Application finished")
 
 
 if __name__ == "__main__":
-    main()
+    TABLE_NAME = "SU-bill"
+    exist_table = check_table_exists(dynamodb_resource(), TABLE_NAME)
+    if not exist_table:
+        create_table(dynamodb_resource(), TABLE_NAME)
+        logger.info("The table has been created")
+        exit()
+    if exist_table:
+        main(TABLE_NAME)
